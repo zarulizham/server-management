b3afc8a47fa8cf92289c82550f27d22ee9c52260
zarulizham <zarul.izham@gmail.com>
2019-04-18 14:24:36 +0800

    Adding pull

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -11,6 +11,17 @@ class WebhookController extends Controller
     {
         Storage::disk('local')->put('abu.txt', json_encode($request->all()));
 
+
+        // $text = '[This] is a [test] string, [eat] my [shorts].';
+        // preg_match_all("/[[^]]*]/", $text, $matches);
+
+
+        exec('/usr/bin/git stash');
+        exec('/usr/bin/git pull');
+
+        // var_dump($matches[0]);
+
+
         echo "K k";
     }
 }
